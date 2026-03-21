416f5dc3d67b04e43a50251b99c71042b2258316
JDIDEK <joss.ddk@gmail.com>
2026-03-21 11:56:10 +0100

    fix: update Vercel Analytics import to use the correct package

--- components/AnalyticsGate.tsx
+++ components/AnalyticsGate.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic'
 import { useCookieConsent } from '@/components/CookieBanner'
 
 const VercelAnalytics = dynamic(
-  () => import('@vercel/analytics/react').then((mod) => mod.Analytics),
+  () => import('@vercel/analytics/next').then((mod) => mod.Analytics),
   { ssr: false },
 )
 
